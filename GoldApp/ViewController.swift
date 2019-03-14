bb2eece1c48c715c3fcb7249ff711459db814aa1
Aleksandra Marzec <olamarzec2@vp.pl>
2019-03-14 20:48:08 +0100

    master

--- GoldApp/ViewController.swift
+++ GoldApp/ViewController.swift
@@ -7,14 +7,77 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    let baseURL = "https://xml.dgcsc.org/samples/SampleGoldJBE.json"
+    
+    let currencyArray = ["AUD", "BRL", "CHF", "CAD", "CNY", "COP", "GBP", "HKD", "IDR", "INR", "JPY", "KWD", "MXN", "MYR", "NZD", "PEN", "PHP", "RUB", "SEK", "SGD", "TRY", "USD", "VUV", "ZAR"]
+    
+    var currencySelected = ""
+    let currencySymbol = ["ƒ", "£", "RD$", "$", "Q", "Ft", "﷼", "₪", "J$", "$", "C$", "₦", "zł", "lej", "Дин", "₨", "CHF", "$U", "₫", "dgj", "₴", "₭", "cos", "cos2" ]
+    var finalURL = ""
+    
+    @IBOutlet weak var goldInPriceLabel: UILabel!
+    
+    @IBOutlet var currencyPicker: UIPickerView!
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return currencyArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return currencyArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        finalURL = baseURL
+        print(finalURL)
+        currencySelected = currencyArray[row]
+        getGoldData(url: finalURL)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        currencyPicker.delegate = self
+        currencyPicker.dataSource = self
     }
-
-
+    
+    func getGoldData(url: String) {
+        
+        Alamofire.request(url, method: .get)
+        .responseJSON { response in
+            if response.result.isSuccess {
+                print("Yes, we have it, Got the gold data")
+                let goldJSON:JSON = JSON(response.result.value!)
+                let cos = goldJSON["GoldPrice"].dictionary
+                let cos2 = cos?["\(self.currencySelected)"]?.dictionary
+                let cos3 = cos2?["bid"]?.string
+                self.updateGoldData(name: cos3)
+            } else {
+                print("Error: \(String(describing: response.result.error))")
+                self.goldInPriceLabel.text = "Connection Issue"
+            }
+        }
+    }
+    
+    func updateGoldData(name: String?) {
+        if let goldResult = name {
+            goldInPriceLabel.text = "\(currencySelected)\(goldResult)"
+        } else {
+            goldInPriceLabel.text = "Price Unavaliable"
+        
+        }
+    }
 }
+
+
+
 
---
+++ Pods/Target Support Files/Pods-GoldApp/Pods-GoldApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GoldApp : NSObject
+@end
+@implementation PodsDummy_Pods_GoldApp
+@end
